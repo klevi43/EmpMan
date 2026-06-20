c9f57f35e28a2aa5bf4907a69f7206de76bda138
Kyle Levisay <kyleandrewlevi43@outlook.com>
2026-06-20 16:57:47 +0900

    Created Department and Employee entities

---
+++ api/Models/Employee.cs
@@ -0,0 +1,14 @@
+namespace api.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; } = DateTime.Now;
+        public DateTime EndDate { get; set; }
+        // Navigation property
+        // allows you to cleanly access the parent entity Dept.Name
+        public required Department Dept { get; set; }
+    }
+}
